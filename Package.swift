5f7dffa9658c78c9989557a0d6322fc095a5a84a
FeliksLv <felikslv@163.com>
2025-01-10 13:14:11 +0800

    build: update tool version

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.3
+// swift-tools-version: 5.4
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -12,7 +12,7 @@ let package = Package(
         .library(
             name: "AppModuleKit",
             type: .static,
-            targets: ["AppModuleKit"]),
+            targets: ["AppModuleKit"])
     ],
     targets: [
         .target(
